1a790d4713f387117f62523d45ab46f6ba15e3ec
unknown <zacksunapollo@gmail.com>
2025-05-04 17:26:27 +0700

    Add: answer exercise 1 4 5

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,6 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
-console.log(newRestaurants);
+for (let newRestaurants in restaurants)
+console.log("Restaurant Name: "+restaurants[newRestaurants]);
+    
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,10 @@
 let companyName = "TechUp";
 
 // Start coding here
+let i = 0;
+for (let char of companyName)
+{i = i+1;
+console.log ("Number " + [i] + " character is " + char)
+};
+//for (let i = 0; i = companyName.length ; i = i+1)
+    //console.log ("Number " + i + "character is " + companyName);
